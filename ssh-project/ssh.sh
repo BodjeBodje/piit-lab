f16fc3567de67853a9157164339c4c111e6e2ef1
BodjeBodje <bouriahdiallo93@gmail.com>
2026-09-17 03:47:23 -0400

    commit add server ip and list server

--- ssh-project/ssh.sh
+++ ssh-project/ssh.sh
@@ -1,7 +1,28 @@
-echo "============server manager============"
-echo "1. Add a server"
-echo "2. list server"
-echo "3. Check server health"
-echo "4. ssh inside a server"
-echo "5. Exit"
-echo "  "
+servers=()
+
+while true; do
+
+    echo "============server manager============"
+    echo "1. Add a server"
+    echo "2. list server"
+    echo "3. Check server health"
+    echo "4. ssh inside a server"
+    echo "5. Exit"
+    echo "  "
+
+    read -p "Make you choice: " choice
+
+    case $choice in
+        1)
+            read -p "enter server ip: " ip
+            servers+=("$ip")
+            echo "server added successfully"
+            ;;
+        2)
+            echo "list server is : "
+            for ip in "${servers[@]}"; do
+                echo "$ip"
+            done
+            ;;
+    esac 
+done
