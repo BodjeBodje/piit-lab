1d54c86aba53523580ac171b4a630f0b3c70de2a
BodjeBodje <bouriahdiallo93@gmail.com>
2026-09-21 01:58:54 -0400

    bank project added

---
+++ scripts/bank-project.sh
@@ -0,0 +1,40 @@
+init_balance=1000
+
+while true; do
+    echo "----------------------------------------"
+    echo "Welcome to simpe Bank"
+    echo "1. Deposit"
+    echo "2. Withdraw"
+    echo "3. Check Balance"
+    echo "4. Exit"
+    echo "----------------------------------------"
+    read -p  "Choose an option: " choose
+    case $choose in
+        1)
+            read -p "Enter amount to deposit: " amount
+            echo "Deposited $amount"
+            deposit=$((init_balance+amount))
+            echo "New Balance: $deposit"
+            ;;
+        2)
+            read -p "Enter amount to withdraw: " withd
+            if [[ $deposit -gt $withd ]]; then
+                deposit=$((deposit-withd))
+                echo "Withdrew $withd"
+                echo "New balance: $deposit"
+            else
+                echo "Insufficient funds!!"
+            fi
+            ;;
+        3)
+            echo "Your current balance: $deposit"
+            ;;
+        4)
+            echo "Goodbye!"
+            exit 0 
+            ;;
+        *)
+            echo "Invalide option!!"
+            echo "Please choose a valid option"
+    esac
+done
