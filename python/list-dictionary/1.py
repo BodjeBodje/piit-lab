aeb51b456f0d63dbcc731cbc1d86181bc30aa657
BodjeBodje <bouriahdiallo93@gmail.com>
2026-09-23 01:31:24 -0400

    Worked on list_dictionary, loops,and fucntion. Some task done

---
+++ python/list-dictionary/1.py
@@ -0,0 +1,11 @@
+
+fruit_list = ["mango","orange","apple"]
+print (type(fruit_list))
+
+print ("------------------------------")
+fruit_list2 = ["mango","orange","apple",124,485,896,925, True]
+print (type(fruit_list2))
+
+print ("------------------------------")
+fruit_list3 = [["mango","orange","apple"],124,485,896,925, True]
+print (fruit_list3[0])
---
+++ python/loops/task.py
@@ -0,0 +1,8 @@
+
+user_pwd=""
+
+
+while user_pwd != "python123":
+    user_pwd = input("Enter you good password: ")
+
+print ("Access granted!!")
